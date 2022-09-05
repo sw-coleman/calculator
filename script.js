d9681993899a047c65f2e75cf8cee5d1a4094108
sw-coleman <samcoleman98@gmail.com>
2022-09-05 14:56:24 +1000

    Minor style changes

--- script.js
+++ script.js
@@ -11,15 +11,24 @@ let currentNum = '';
 let previousNum = '';
 let result = null;
 let operation = '';
+let inverseNumber = '';
 let hasDecimal = false;
+let alreadyCalc = false;
 
 //Add event listener to each digit button, and change display to show value of button
 digitBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
+        limitUserInput();
+        if (limitUserInput() === true){
+            return;
+        }
         if(e.target.textContent === '.' && !hasDecimal){ //Only allow the user to add 1 decimal point to each number
             hasDecimal = true;
         } else if (e.target.textContent === '.' && hasDecimal){
             return;
+        }
+        if (alreadyCalc === true){ //If calculation has already been complete, new input will overwrite previous reuslt.
+            clearDisplay();
         }
         const value = e.target.dataset.value;
         displayMain.textContent += value;
@@ -29,37 +38,45 @@ digitBtns.forEach(btn => {
 //Add event listener to each operator button
 operatorBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        if(!currentNum) result;
+        if(!currentNum) return;
         hasDecimal = false;
         const operationName = e.target.textContent;
         if(previousNum && currentNum && operation){
-            twoInputs = true;
+            operate();
         } else {
             result = parseFloat(currentNum);
         }
-        console.log(result);
         clearNum(operationName);
         operation = operationName;
+        alreadyCalc = false;
     });
 });
 
+//Functionality for equals button.
 equalsBtn.addEventListener("click", (e)=> {
-    if(!currentNum || !previousNum) return;
+    if(!currentNum || !result) return;
+    if(alreadyCalc === true) return;
     hasDecimal = false;
     operate();
     clearNum();
-    displayMain.textContent = result;
+    if (result.toString().length < 10) {
+        displayMain.textContent = result;
+    } else {
+        displayMain.textContent = result.toPrecision(5);
+    }
     displaySec.textContent = '';
     currentNum = result;
     previousNum = '';
+    alreadyCalc = true;
 });
 
 //Function to clear current var and store in secondary var
 function clearNum(name = '') {
-    previousNum += currentNum + '' + name + ' ';
-    displaySec.textContent = previousNum;
+    displaySec.textContent = currentNum + '' + name + ' ';
+    previousNum = result;
     hasDecimal = false;
-    displayMain.textContent = '';
+    displayMain.textContent = ''
+    currentNum = '';
 }
 
 //Arithmatic functions
@@ -76,19 +93,27 @@ function divide(x,y){
     if (y!=0){
         return x / y;
     } else {
-        display.textContent = "ERROR"; //Cannot divide a number by 0.
+        return "ERROR"; //Cannot divide a number by 0.
     }
 }
+function modulus(x,y){
+    return x % y;
+}
+
+//Function to add negative/positive button
+posNegBtn.addEventListener("click", (e)=> {
+
+});
 
 //Function to clear display 
 function clearDisplay() {
     currentNum = '';
     previousNum = '';
+    result = '';
     displayMain.textContent = '';
     displaySec.textContent = '';
-    result = '';
+    alreadyCalc = false;
 }
-
 clearBtn.addEventListener("click", clearDisplay);
 
 //Function to execute arithmatic operation
@@ -101,5 +126,17 @@ function operate(){
         result = subtract(parseFloat(result), parseFloat(currentNum));
     } else if (operation === '÷'){
         result = divide(parseFloat(result), parseFloat(currentNum));
+    } else if (operation === '%'){
+        result = modulus(parseFloat(result), parseFloat(currentNum));
     }
 }
+
+//Function to limit user input to 6 digits
+function limitUserInput(){
+        if (currentNum.toString().length > 10){
+            return true;
+        } else {
+            return false;
+        }
+};
+
